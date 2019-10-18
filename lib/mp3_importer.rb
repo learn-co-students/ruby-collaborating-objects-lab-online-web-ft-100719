a2794b5d93b6f7a7f76ec5cbf2487227461f74de
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-18 19:32:37 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,16 +1,29 @@
-class Mp3Importer
+class MP3Importer
   attr_accessor :path, :filename 
   
-  def initialize(path="./spec/fixtures/")
+  #path ="./spec/fixtures/mp3s")
+  def initialize(path)
     @path = path 
   end #initialize
+  
   def import 
+    self.files.each do |filename|
+      file = filename.gsub(/.mp3$/, '')
+      Song.new_by_filename(file)
+    end
   end #import
   
   def files
-  end
+    files = []
+    
+    Dir.glob(@path + '/*.mp3').each do |f|
+      files << f.split("/").last
+    end #each
+   
+    files
+
+  end #files
   
 end # Mp3Importer
 
 
-Dir.glob("#{@path}*.mp3") 
--- lib/song.rb
+++ lib/song.rb
@@ -12,8 +12,21 @@ class Song
     @@all
   end
   
-  def artist_name
+  def self.artist_name
     @artist ? @artist.name : NIL
+  end
+  
+  def self.artist_name=(name)
+    @artist = Artist.find_or_create_by_name(name)
+  end
+  
+  def self.new_by_filename(filename)
+    song_details = filename.gsub(/.mp3$/, '').split(" - ")
+    song_name = song_details[1]
+    new_song = self.new(song_name)
+    new_song.artist = Artist.find_or_create_by_name(song_details[0])
+    artist_name= song_details[0]
+    new_song
   end
   
 end
