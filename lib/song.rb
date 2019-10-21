f19056dc80fb68471bef793de6ed9bc49004ed1b
Jonathan Norman <jnorman2123@gmail.com>
2019-10-21 00:04:28 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+require 'pry'
+class Song
+  attr_accessor :name, :artist
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.new_by_filename(filename)
+    split_file = filename.split(" - ")
+    song = self.new(split_file[1])
+    artist = Artist.find_or_create_by_name(split_file[0])
+    song.artist = artist
+    artist.add_song(song)
+    song
+  end
+
+  def artist_name=(name)
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist
+  end
+end
