683c3ac850b181adfc8b0f5cec7854939c53827f
Cristal Alatorre Perez <cristalcodes@gmail.com>
2019-10-23 05:09:14 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,12 +23,20 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    if (self.artist.nil?)
-      self.artist=Artist.new(name)
-    elsif
-      self.artist.name=name
+    artist=self.all.find{|artist| artist.name==name}
+    if artist.nil?
+      artist=Artist.new(name)
+    else
+      artist
     end
+  end
 
+  def print_songs
+    Song.all.select do |song|
+      if song.artist==self
+        puts song.name
+      end
+    end 
   end
 
 
--- lib/song.rb
+++ lib/song.rb
@@ -21,21 +21,16 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    song=Song.new(@filename)
-    song.name=filename.split(' - ')[1]
-    song.artist=filename.split(' - ')[0]
-    # song = self.new
-    # song.name = filename.split(" - ")[1]
-    # song
-
-
-    # song=self.new
-    # song.name=filename.split(' - ')[1]
-    #
-
-    # song.artist=filename.split(' - ')[0]
-
+    @filename=filename.split(' - ')
+    song=Song.new(@filename[1])
+    song.artist_name=(@filename[0])
+    song
   end
 
+  def artist_name=(name)
+    @artist=Artist.find_or_create_by_name(name)
+  end
+
+  
 
 end
